6eeb420d9febdf96e5114a677c70db6267895c57
vikas jat <vjchoudhary1234@gmail.com>
2023-01-16 18:09:12 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
-public class Main {
+import java.util.PrimitiveIterator;
 
+public class Main {
+    public  static class Product{
+        public static int product(int x, int y ){
+            return x*y;
+        }
+        public static  int product(int x, int y , int z){
+            return x*y*z;
+        }
+        public static double product (double x, double y){
+            return  x*y;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(6,7);
+        p.product(7,8,9);
+        p.product(5.0,4.0);
+    }
 }
